ab248d60a25be3b5bb65da363aff1887085dd226
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-07 21:24:02 +0600

    create model and dbContext file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MiniAccountManagementSystem.Models;
+
+namespace MiniAccountManagementSystem.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace MiniAccountManagementSystem.Models;
+public class ApplicationUser : IdentityUser
+{
+    
+}
