54e81730996e0190bb65e61d06bce1dd2cf9a97e
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-08 21:14:48 +0600

    add list to ui

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -9,13 +9,37 @@ namespace MiniAccountManagementSystem.Controllers;
 [Authorize(Roles = "Admin,Accountant")]
 public class AccountController : Controller
 {
-    public AccountController()
+    private readonly IConfiguration _configuration;
+    public AccountController(IConfiguration configuration)
     {
-
+        _configuration = configuration;
     }
     public IActionResult ChartOfAccount()
     {
-        return View();
+        List<AccountModel> accounts = new();
+        string connStr = _configuration.GetConnectionString("DefaultConnection")!;
+
+        using (var conn = new SqlConnection(connStr))
+        {
+            using (var cmd = new SqlCommand("SELECT A.AccountId, A.AccountName, A.ParentId, P.AccountName AS ParentName FROM ChartOfAccounts A LEFT JOIN ChartOfAccounts P ON A.ParentId = P.AccountId", conn))
+            {
+                conn.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        accounts.Add(new AccountModel
+                        {
+                            AccountId = Convert.ToInt32(reader["AccountId"]),
+                            AccountName = reader["AccountName"].ToString()!,
+                            ParentId = reader["ParentId"] as int?,
+                            ParentName = reader["ParentName"]?.ToString()
+                        });
+                    }
+                }
+            }
+        }
+        return View(accounts);
     }
     
     public IActionResult VoucherList()
