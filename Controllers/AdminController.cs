b1f22483150e298b67dbc8d1352f46e39ce00653
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-08 07:21:02 +0600

    setting up for authorized

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniAccountManagementSystem.Controllers;
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MiniAccountManagementSystem.Models;
 
 namespace MiniAccountManagementSystem.Controllers;
 
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
